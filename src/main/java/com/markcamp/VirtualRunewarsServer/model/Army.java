9e71f69446750132fbb225e43c3e0dfd2f53dd2b
Mark <markcampbell_88@yahoo.com>
2020-05-22 20:59:29 -0400

    initial commit

---
+++ src/main/java/com/markcamp/VirtualRunewarsServer/model/Army.java
@@ -0,0 +1,16 @@
+package com.markcamp.VirtualRunewarsServer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public abstract class Army {
+
+    @Getter
+    @Setter
+    public String race;
+    @Getter
+    @Setter
+    public String name;
+
+
+}
---
+++ src/main/java/com/markcamp/VirtualRunewarsServer/model/Reanimates.java
@@ -0,0 +1,10 @@
+package com.markcamp.VirtualRunewarsServer.model;
+
+public class Reanimates extends Army{
+
+    public Reanimates()
+    {
+        this.name = "Reanimates";
+        this.race = "Waiqar";
+    }
+}
